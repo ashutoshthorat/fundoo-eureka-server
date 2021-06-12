21056d8e0001f502f412c7d247002e1a68da0398
ashutoshthorat <ashutosh.thorat@bridgelabz.com>
2021-06-12 08:12:59 +0530

    added Service discovery using eureka server

---
+++ src/test/java/com/bridgelabz/fundooregistry/FundooregistryApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.bridgelabz.fundooregistry;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class FundooregistryApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
